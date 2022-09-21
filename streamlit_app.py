3f451cc952b5a323fd2bdd8fe7cfd3c3f91a2bb5
yishi0 <108908908+yishi0@users.noreply.github.com>
2022-09-21 13:38:18 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())  #just writes the data to the screen
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json())  #just writes the data to the screen
 
 # json形式をノーマライズ化
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
