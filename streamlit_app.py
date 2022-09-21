3eb7258d5a791b08acf5d68b3301f4d5fc0e0360
yishi0 <108908908+yishi0@users.noreply.github.com>
2022-09-21 17:46:34 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,3 +35,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # テーブル形式で表示
 streamlit.dataframe(fruityvice_normalized)
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
