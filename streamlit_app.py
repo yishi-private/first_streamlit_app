55d156984402325a1c6175922fd6531e18e06e5e
yishi0 <108908908+yishi0@users.noreply.github.com>
2022-09-21 17:41:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # テーブル形式で表示
 streamlit.dataframe(fruityvice_normalized)
+import snowflake.connector
