c7bc0c19a002bd19313574ab030dc2999a8ccbf5
yishi0 <108908908+yishi0@users.noreply.github.com>
2022-09-21 15:17:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,29 +7,29 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+# import pandas
+# my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Acocado','Strawberries'])
-#fruits_to_show = my_fruit_list.loc[fruits_selected]
+# # Let's put a pick list here so they can pick the fruit they want to include 
+# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Acocado','Strawberries'])
+# #fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
-#New Section to display fruityvice api response
-streamlit.header('Fruityvice Fruit Advice!')
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', fruit_choice)
+# # Display the table on the page.
+# streamlit.dataframe(fruits_to_show)
+# #New Section to display fruityvice api response
+# streamlit.header('Fruityvice Fruit Advice!')
+# #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+# #streamlit.write('The user entered ', fruit_choice)
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())  #just writes the data to the screen
+# import requests
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# streamlit.text(fruityvice_response.json())  #just writes the data to the screen
 
-# json形式をノーマライズ化
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# テーブル形式で表示
-streamlit.dataframe(fruityvice_normalized)
+# # json形式をノーマライズ化
+# fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# # テーブル形式で表示
+# streamlit.dataframe(fruityvice_normalized)
